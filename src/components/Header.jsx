5583572e93d697667418077d95417f284edb2fd5
Vinicius_siqueira098 <viniv908@gmail.com>
2025-06-24 23:30:08 -0300

    Substituindo conteúdo do repositório

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="header" style={{ background: 'red' }}>
+            <div className="container">
+           
+            <form>
+                <input 
+                type="search" 
+                placeholder="Buscar produtos" 
+                className="search__input" 
+                required
+                />
+
+                <button type="submit" className="search__button">
+                    icone
+                </button>    
+            </form>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
